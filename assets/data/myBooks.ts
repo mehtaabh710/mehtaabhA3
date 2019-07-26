c407ef04779f06a5f58e9b405418f7d887dbc04a
mehtaabh710 <52579165+mehtaabh710@users.noreply.github.com>
2019-07-26 16:20:30 -0400

    Add files via upload

---
+++ assets/data/myBooks.ts
@@ -0,0 +1,7 @@
+import{BookInfo}from '../../app/BookInfo';
+export const BOOKS: BookInfo[] = [
+    {book_name:"The Hunger Games",book_author:"Suzanne Collins",book_genre:"Utopian and dystopian fiction, Science Fiction, Drama, Action fiction",book_pyear:"September 14, 2008",book_picture:"assets/images/1.jpg"},
+    {book_name:"Harry Potter and the Order of the Phoenix",book_author:"J. K. Rowling",book_genre:"Fantasy",book_pyear:"June 21, 2003",book_picture:"assets/images/2.jpg"},
+    {book_name:"To Kill a Mockingbird",book_author:"Harper Lee",book_genre:"Southern Gothic, Bildungsroman",book_pyear:"July 11, 1960",book_picture:"assets/images/3.jpg"},
+    {book_name:"The Chronicles of Narnia",book_author:"C. S. Lewis",book_genre:"Fantasy, Children's literature",book_pyear:"September 16th 2002",book_picture:"assets/images/4.jpg"}
+]
